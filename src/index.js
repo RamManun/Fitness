03d7996c91fd99c479e141e8c67cf7ea6d6a3c55
RamManun <Ramces12@gmail.com>
2020-03-22 21:34:02 -0600

    listo

--- src/index.js
+++ src/index.js
@@ -9,13 +9,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './Components/Card'
+import Welcome from './Components/Welcome'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card
+ReactDOM.render(<div>
+    <Welcome
+        username="Ram"
+    />
+    <Card
                     title="Mi primera practica"
                     description="Hola React llego tu papa"
                     img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
                     letfColor="#A74CF2"
                     righColor="#617BFB"
-                />, container)
+    />
+    </div>,container)
